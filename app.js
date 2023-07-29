e898a6b22d84588d605c8866e08c4926c186bf96
Voloshun555 <voloshunihor@gmail.com>
2023-07-29 15:58:00 +0300

    789

--- app.js
+++ app.js
@@ -2,9 +2,7 @@ const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
 
-
-
-
+require("dotenv").config();
 
 const app = express();
 
@@ -13,8 +11,6 @@ const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
-
-
 
 app.use((req, res) => {
   res.status(404).json({ message: "Not found" });
--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 const app = require("./app");
-require("dotenv").config()
-const mongoose = require("mongoose");
-const {DB_HOST} = process.env;
 
-  mongoose.set('strictQuery', true)
+const mongoose = require("mongoose");
+const { DB_HOST } = process.env;
+
+mongoose.set("strictQuery", true);
 
 mongoose
   .connect(DB_HOST)
   .then(() => {
     app.listen(3000);
   })
-  .catch(error => {
+  .catch((error) => {
     console.log(error.message);
     process.exit(1);
   });
