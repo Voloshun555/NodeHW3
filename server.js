cc32684d8ec944bb81415c39e6a73ab4646e11e0
Voloshun555 <voloshunihor@gmail.com>
2023-07-29 11:43:35 +0300

    888

--- server.js
+++ server.js
@@ -4,13 +4,14 @@ const mongoose = require("mongoose");
 
 const DB_HOST =
   "mongodb+srv://Voloshun:z4CYyKzniR1O7dcn@cluster0.aknwarz.mongodb.net/Contacts_reader?retryWrites=true&w=majority";
+  mongoose.set('strictQuery', true)
 
 mongoose
   .connect(DB_HOST)
   .then(() => {
     app.listen(3000);
   })
-  .catch((error) => {
+  .catch(error => {
     console.log(error.message);
     process.exit(1);
   });
