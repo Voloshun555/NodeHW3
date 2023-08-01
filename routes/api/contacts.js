28a12ac1a703dfdd1d0bee1adfe9d8178557a58c
Voloshun555 <voloshunihor@gmail.com>
2023-08-01 16:11:35 +0300

    555

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const ctrl = require("../../controllers/contacts");
 
-const { validateBody } = require("../../middlewares");
-const schemas = require("../../schemas/contacts");
+// const { validateBody } = require("../../middlewares");
+// const schemas = require("../../schemas/contacts");
 
 const router = express.Router();
 
--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 const app = require("./app");
 
 const mongoose = require("mongoose");
-const { DB_HOST } = process.env;
+const { DB_HOST, PORT = 3000 } = process.env;
 
 mongoose.set("strictQuery", true);
 
 mongoose
   .connect(DB_HOST)
   .then(() => {
-    app.listen(3000);
+    console.log("Database connection successful")
+    app.listen(PORT);
   })
   .catch((error) => {
     console.log(error.message);
